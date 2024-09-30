fdceecc7458e8a5e952cc12897d72284bc54a799
sujith-mile9 <sujith@mile9.io>
2024-09-30 13:30:44 +0530

    server chamges

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,7 @@ connectDB()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
-app.get('/', (req,res)=>{
-    res.send('api is running....!')
-})
+
 
 app.use('/api/products',productsRouters)
 app.use('/api/users',useRoutes)
@@ -37,10 +35,23 @@ app.get('/api/config/paypal', (req, res) =>
 
 
   const __dirname = path.resolve();
-  app.use('/uploads', express.static('/var/data/uploads'));
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+ 
 
-  
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+
+  }
+  else {
+    app.get('/', (req,res)=>{
+      res.send('api is running....!')
+  })
+  }
+
 app.use(notFound)
 app.use(errorHandler)
 app.listen(port, ()=>console.log(`Server running on ${port}`))
