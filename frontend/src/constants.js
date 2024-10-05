848e7f1422ffa344c9a75c05485348eb55aff17c
sujith-mile9 <sujith@mile9.io>
2024-10-06 00:18:29 +0530

    azure ACa base url update

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -4,7 +4,7 @@
 
 // export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : ''
 
-export const BASE_URL = ''
+export const BASE_URL = 'https://proshopaca.proudflower-d9e2f946.centralindia.azurecontainerapps.io'
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
